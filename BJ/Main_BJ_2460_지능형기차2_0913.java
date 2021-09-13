3296836bbdbbff40bab3419a661845ba46ce6363
HwangSoojin <blue8959@naver.com>
2021-09-13 20:15:17 +0900

    백준 2460 지능형기차2

---
+++ BJ/Main_BJ_2460_지능형기차2_0913.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main_BJ_2460_지능형기차2_0913 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int res = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < 10; i++) {
+			sum = sum - sc.nextInt() + sc.nextInt();
+			res = Math.max(res, sum);
+		}
+		
+		System.out.println(res);
+		sc.close();
+	}
+
+}
