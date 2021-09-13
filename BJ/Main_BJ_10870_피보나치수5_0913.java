282f1bcb1fc7ba16cb84cdea38db20008768b833
HwangSoojin <blue8959@naver.com>
2021-09-13 20:16:02 +0900

    백준 10870 피보나치수5

---
+++ BJ/Main_BJ_10870_피보나치수5_0913.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main_BJ_10870_피보나치수5_0913 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(fibo(sc.nextInt()));
+	}
+
+	private static int fibo(int n) {
+		if(n <= 1)
+			return n;
+		else return fibo(n-1) + fibo(n-2);		
+	}
+}
