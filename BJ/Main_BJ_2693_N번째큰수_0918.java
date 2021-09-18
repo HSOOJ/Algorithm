7a31671400d6b56daab9704d162d2c2748b80dc1
HwangSoojin <blue8959@naver.com>
2021-09-18 16:56:25 +0900

    백준 2693 N번째 큰 수

---
+++ BJ/Main_BJ_2693_N번째큰수_0918.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_BJ_2693_N번째큰수_0918 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int arr[] = new int[10];
+			
+			for (int i = 0; i < 10; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(arr);
+			
+			System.out.println(arr[7]);
+		}
+	}
+
+}
