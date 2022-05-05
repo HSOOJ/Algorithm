f7bed11eb05287296b06a76055a9c89ee6c36214
HwangSoojin <blue8959@naver.com>
2022-05-05 15:58:51 +0900

    [L2] 124 나라의 숫자

---
+++ PG/L2_Solution_13704_124나라의숫자_220505.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 3) {
+            int tmp = n % 3;
+            if(tmp == 0) {
+                tmp = 4;
+                sb.insert(0, tmp);
+                n = n / 3 - 1;
+            } else {
+                sb.insert(0, n % 3);
+                n /= 3;
+            }
+        }
+        
+        int add = n;
+        if(n == 3)
+            add = 4;
+        
+        sb.insert(0, add);
+            
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
