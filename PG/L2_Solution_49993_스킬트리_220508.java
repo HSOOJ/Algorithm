cb9211542f76c29af7f4250821b7b06ed921b4b4
HwangSoojin <blue8959@naver.com>
2022-05-08 16:29:41 +0900

    [L2] 49993 스킬트리

---
+++ PG/L2_Solution_49993_스킬트리_220508.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+
+        for(int i = 0; i < skill_trees.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            
+            for(int j = 0; j < skill_trees[i].length(); j++) {
+                String s = Character.toString(skill_trees[i].charAt(j));
+                if(skill.contains(s))
+                    sb.append(s);
+            }
+
+            if(sb.toString().equals(skill))
+                answer++;
+            else {
+                if(sb.length() < skill.length())
+                    if(skill.substring(0, sb.length()).equals(sb.toString()))
+                        answer++;
+            }
+        }
+        return answer;
+    }
+}
